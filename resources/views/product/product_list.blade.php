5b334a21ef3b535047a0853f9a4d1878365684d7
John <chachat_2015@hotmail.com>
2020-02-01 15:32:53 +0700

    create structure of project patong

--- resources/views/product/product_list.blade.php
+++ resources/views/product/product_list.blade.php
@@ -7,6 +7,23 @@
             <h2>Product List</h2>
             <p>What our client say about us</p>
         </div><!--/.section-header-->
+        <div class="container" style="padding-top: 50px">
+            <div class="row">
+                <div class="col-sm-3">
+                    @for($index = 0; $index <= 5; $index++)
+                        <div>list menu {{ $index + 1 }}</div>
+                    @endfor
+                </div>
+                <div class="col-sm-9">
+                    <div class="row">
+
+                        @for($index = 0; $index <= 5; $index++)
+                        <div class="col-sm-3">product {{ $index + 1 }}</div>
+                        @endfor
+                    </div>
+                </div>
+            </div>
+        </div><!--/.section-header-->
     </section><!--/.reviews-->
     <!--reviews end -->
 @endsection
